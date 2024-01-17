59a5568a996ca86a3a0a4cd7dec742e3bd40b85e
Demo1744 <demo52688@gmail.com>
2024-01-17 11:18:52 +0530

    modified:   Backend/server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,12 @@
-console.log("kiddo")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.status(200).json( {message: 'Welcome to the E-Ticket Backend'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
